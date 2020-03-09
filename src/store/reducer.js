6d8e13bef15327395a64f77dabc4cd0904245056
agamvrinos <agg_gam@hotmail.com>
2020-03-09 22:47:54 +0100

    Improve reducer readability

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -12,6 +12,39 @@ const initialState = {
     totalMatches: 0
 }
 
+const incrementCaret = (state, action) => {
+    return {
+        ...state,
+        caretIndex: state.caretIndex + 1
+    };
+}
+
+const decrementCaret = (state, action) => {
+    return {
+        ...state,
+        caretIndex: state.caretIndex - 1
+    };
+}
+
+const updateInput = (state, action) => {
+    let count = 0;
+    if (action.input) {
+        state.output.forEach(out => {
+            const parts = getSplitParts(out, action.input, state.matchCase);
+            parts.forEach(part => {
+                if (part.toLowerCase() === action.input.toLowerCase()) {
+                    count += 1;
+                }
+            })
+        })
+    }
+    return {
+        ...state,
+        input: action.input,
+        totalMatches: count
+    };
+}
+
 const getSplitParts = (text, highlight, matchCase) => {
     let regex = new RegExp(`(${highlight})`, "gi");
     if (matchCase) {
@@ -22,35 +55,10 @@ const getSplitParts = (text, highlight, matchCase) => {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case (actions.INCREMENT_CARET): 
-            return {
-                ...state,
-                caretIndex: state.caretIndex + 1
-            };
-        case (actions.DECREMENT_CARET): 
-            return {
-                ...state,
-                caretIndex: state.caretIndex - 1
-            };
-        case (actions.UPDATE_INPUT):
-            let count = 0;
-            if (action.input) {
-                state.output.forEach(out => {
-                    const parts = getSplitParts(out, action.input, state.matchCase);
-                    parts.forEach(part => {
-                        if (part.toLowerCase() === action.input.toLowerCase()) {
-                            count += 1;
-                        }
-                    })
-                })
-            }
-            return {
-                ...state,
-                input: action.input,
-                totalMatches: count
-            };
-        default: 
-            return state;
+        case (actions.INCREMENT_CARET): return incrementCaret(state, action);
+        case (actions.DECREMENT_CARET): return decrementCaret(state, action);
+        case (actions.UPDATE_INPUT): return updateInput(state, action);
+        default: return state;
     }
 }
 
